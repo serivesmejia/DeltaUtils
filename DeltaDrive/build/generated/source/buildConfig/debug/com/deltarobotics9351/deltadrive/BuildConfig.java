a22b09a233920b754b470aee12d5fbc333303b84
serivesmejia <serivesmejia@gmail.com>
2019-12-24 13:58:19 -0700

    added dependencies again

---
+++ DeltaDrive/build/generated/source/buildConfig/debug/com/deltarobotics9351/deltadrive/BuildConfig.java
@@ -0,0 +1,18 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.deltarobotics9351.deltadrive;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "com.deltarobotics9351.deltadrive";
+  /**
+   * @deprecated APPLICATION_ID is misleading in libraries. For the library package name use LIBRARY_PACKAGE_NAME
+   */
+  @Deprecated
+  public static final String APPLICATION_ID = "com.deltarobotics9351.deltadrive";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}

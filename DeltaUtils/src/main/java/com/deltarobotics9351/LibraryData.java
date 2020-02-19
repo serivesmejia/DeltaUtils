55a5aa785172a77b67d1f09ec1aabfb75e87cbe2
Sebastian Erives Mejia <serivesmejia@gmail.com>
2020-02-18 20:07:32 -0700

    correction: 1.10-beta

--- DeltaUtils/src/main/java/com/deltarobotics9351/LibraryData.java
+++ DeltaUtils/src/main/java/com/deltarobotics9351/LibraryData.java
@@ -2,7 +2,7 @@ package com.deltarobotics9351;
 
 public class LibraryData {
 
-    public static String VERSION = "1.11-beta";
-    public static int RELASE_NUMBER = 11;
+    public static String VERSION = "1.10-beta";
+    public static int RELASE_NUMBER = 13;
 
 }
